4ed3b150ce157ef0857d6dda3a56ff1b168c6433
Zdravko Jovancevic <jovanceviczdravko95@gmail.com>
2022-04-16 18:26:00 +0200

    changes

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Create a Flask Instance
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "my super secret key"
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+	name = StringField("What's Your Name", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
 
 # Create a route decorator
 
@@ -52,3 +63,24 @@ def page_not_found(e):
 def page_not_found(e):
 	return render_template("500.html"), 500	
 		
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+
+def name():
+	name = None
+	form = NamerForm()
+	# Validate Form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	
+	return render_template("name.html",
+		name=name,
+		form=form)
+
+
+
+
+
+
